76ffa87e1058c070bf12270f0844a438c73a64f1
Jaanek Oja <jaanek@oja.me>
2018-07-13 12:30:28 +0300

    Retry method call on known errors

--- common/web3_utils.js
+++ common/web3_utils.js
@@ -41,18 +41,209 @@ logger.error.log = console.error.bind(console);
     });
 }
 
- async function sendTransactionAndWaitForReceiptMined (web3, method, options) {
+async function sendTransactionAndWaitForReceiptMined(web3, method, options) {
     let args = Array.from(arguments).slice(3);
     let gasSafetyMargin = 1.05;
     args.push(options);
     let gasEstimate = await method.estimateGas.apply(null, args);
     options.gas = Math.ceil(Number(gasEstimate) * gasSafetyMargin);
-    logger.info(`Gas estimated ${gasEstimate}, using ${options.gas}`);
-    let tx = await method.apply(null, args);
+    console.info(`Gas estimated ${gasEstimate}, using ${options.gas}`);
+    const methodWithArgs = method.bind(null, ...args);
+    const tx = await retryCallOnError(web3, methodWithArgs);
     logger.info(`Waiting for transaction ${tx.receipt.transactionHash} to be mined`);
     tx.receiptMined = await getTransactionReceiptMined(web3, tx.receipt.transactionHash);
     logger.info(`Transaction is mined @${tx.receiptMined.blockNumber}`);
     return tx;
+}
+
+async function callMethodCheckKnownError(method) {
+  let result;
+  try {
+    result = await method();
+  } catch (err) {
+    // check for known error
+
+    // GETH
+    // {"jsonrpc":"2.0","id":1531402827926133,"error":{"code":-32000,"message":"replacement transaction underpriced"}}
+    // {"jsonrpc":"2.0","id":1531403520972173,"error":{"code":-32000,"message":"known transaction: 9fd3d601f1d7c3952fdc9cf4ddf04e629e507bc82f08929c59da4139ec512a27"}}
+    // Parity
+    // {"jsonrpc":"2.0","error":{"code":-32010,"message":"Transaction gas price is too low. There is another transaction with same nonce in the queue. Try increasing the gas price or incrementing the nonce."},"id":1531471891913099}
+    // {"jsonrpc":"2.0","error":{"code":-32010,"message":"Transaction with the same hash was already imported."},"id":1531473426794689}
+
+    // if known error, then return the error
+    if (err && err.message) {
+      const message = err.message.toLowerCase();
+      // logger.info(`Error message: ${message}`);
+      const knownError = (
+          message.indexOf('replacement transaction') >= 0 ||
+          message.indexOf('known transaction') >= 0 ||
+          message.indexOf('there is another transaction with same nonce in the queue') >= 0 ||
+          message.indexOf('transaction with the same hash was already imported') >= 0
+      )
+      if (knownError) {
+        return {
+          error: err
+        }
+      }
+    }
+    // unknown error, rethrow
+    throw err;
+  }
+  return {
+    result: result
+  };
+}
+
+const MAX_BLOCK_RETRIES = 5;
+async function retryCallOnError(web3, method) {
+  return new Promise(async (resolve, reject) => {
+    const provider = web3.currentProvider.provider;
+
+    // initialize current block number if available
+    let currentBlockNumber;
+    if (provider.currentBlock) {
+      try {
+        currentBlockNumber = ethutils.bufferToInt(provider.currentBlock.number);
+      } catch (error) {
+        return reject(error);
+      }
+    }
+
+    // check retries
+    let blockNumbers = [];
+    let retriesAllowed = false;
+    let retriesCount = 0;
+    async function retry() {
+      if (!retriesAllowed) {
+        // logger.info(`Already retrying a method!`);
+        return;
+      }
+      retriesAllowed = false;
+
+      // select the higher block
+      let oldBlockNumber = currentBlockNumber;
+      for (let i=0; i < blockNumbers.length; i++) {
+        const blockNumber = blockNumbers[i];
+        if (!currentBlockNumber) {
+          currentBlockNumber = blockNumber;
+        }
+        if (blockNumber > currentBlockNumber) {
+          currentBlockNumber = blockNumber;
+        }
+      }
+      // clear the array so that it can start accumulating again
+      blockNumbers = [];
+
+      // nothing to do if not a new block number
+      if (currentBlockNumber <= oldBlockNumber) {
+        retriesAllowed = true;
+        logger.info(`Waiting for next block!`);
+        return;
+      }
+
+      // otherwise retry
+      logger.info(`Retrying a method! currentBlockNumber: ${currentBlockNumber}, retriesCount: ${retriesCount}`);
+      try {
+        const {result, error} = await callMethodCheckKnownError(method);
+        if (error) {
+          // check if we have reached max retries
+          retriesCount++;
+          if (retriesCount > MAX_BLOCK_RETRIES) {
+            finish(null, new Error('Max retried blocks reached!'));
+            return;
+          }
+
+          // known error, allow retries
+          logger.info(`Known error! Retry count: ${retriesCount}. Error: ${error}`);
+          retriesAllowed = true;
+
+          // trigger retries here if 'nextBlock' events have already accumulated some new blocks
+          if (blockNumbers.length) {
+            retry().catch((err) => {
+              // should not happen
+              logger.info(`Retry error!`, err);
+            });
+          }
+          return;
+        }
+        finish(result);
+      } catch (err) {
+        // unknown error
+        finish(null, err);
+      }
+    }
+
+    // provide a method here to clean up on exit
+    let timeoutId;
+    function finish(result, error) {
+      logger.info(`Finishing up! result: ${JSON.stringify(result)}, error: ${JSON.stringify(error)}`);
+      // clear the resources
+      retriesAllowed = false;
+      provider.engine.off('block', nextBlockListener);
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+
+      // return result
+      if (error) {
+        return reject(error);
+      }
+      resolve(result);
+    }
+
+    // accumulate the block number's here that come in from 'nextBlock' listener
+    function nextBlockNumberListener(blockNumber) {
+      logger.info(`Got blockNumber: `, blockNumber);
+      blockNumbers.push(blockNumber);
+
+      // we try to retry if new blocks are coming in
+      retry().catch((err) => {
+        // should not happen
+        logger.info(`Retry error!`, err);
+      });
+    }
+
+    // register next block listener immediately
+    function nextBlockListener(nextBlock) {
+      nextBlockNumberListener(ethutils.bufferToInt(nextBlock.number));
+    }
+    provider.engine.on('block', nextBlockListener);
+
+    // call method, if known error, then retry
+    try {
+      const {result, error} = await callMethodCheckKnownError(method);
+      if (error) {
+        // known error, allow retries
+        logger.info(`Known error! ${error}, blockNumbers: ${blockNumbers}`);
+        retriesAllowed = true;
+
+        // trigger retries here if 'nextBlock' events have already accumulated some new blocks
+        if (blockNumbers.length) {
+          retry().catch((err) => {
+            // should not happen
+            logger.info(`Retry error!`, err);
+          });
+        } else {
+          // just in case, trigger current block fetch in case we missed nextBlock somehow
+          web3.eth.getBlockNumber((err, blockNumber) => {
+            nextBlockNumberListener(blockNumber);
+          });
+        }
+
+        // start the timer in case the nextBlock's do not come in for some reason or
+        // next blocks come in very slowly
+        timeoutId = setTimeout(() => {
+          finish(null, new Error('Timeout when calling/retrying a method!'));
+        }, 5 * 60 * 1000); // 5 mins
+        return;
+      }
+      finish(result);
+    } catch (err) {
+      // unknown error
+      finish(null, err);
+    }
+  });
 }
 
 module.exports = {
